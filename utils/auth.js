2fb5b146b64ec23634f54cd4b87790d9d8d22969
aj95-nair <aj95.nair@gmail.com>
2022-07-25 22:12:53 +0400

    new

---
+++ utils/auth.js
@@ -0,0 +1,50 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const {SECRET} = require('../config');
+
+const userlogin = async (userCred ,res) => {
+    let {username, password} = userCred;
+
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(404).json({
+            message: "USER NOT FOUND",
+            success: false
+        });
+    }
+
+    let passwordmatch = await bcrypt.compare(password, user.password);
+
+    if(passwordmatch){
+
+        let token = jwt.sign({
+            _id: user._id, 
+            role: user.role,
+            email:user.email
+        }, SECRET, {expiresIn: "7 days"});
+
+        let authtoken = {
+            _id: user._id,
+            role: user.role,
+            email: user.email,
+            token: `${token}`,
+            expiresIn: 168
+        };
+        return res.status(200).json({
+            authtoken,
+            message: `${user.role} SUCESS`,
+            success: true
+        });
+
+    }else{
+        return res.status(404).json({
+            message: "WRONG PASSWORD",
+            success: false
+        });
+    }
+}
+
+module.exports = {
+    userlogin
+}
